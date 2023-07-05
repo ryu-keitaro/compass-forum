ea447caec159587fae3e264079cd6c5d341f7b31
ryu-keitaro <dragon131020@gmail.com>
2023-07-05 16:35:08 +0900

    Merge remote changes

--- src/_app.tsx
+++ src/_app.tsx
@@ -16,10 +16,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 				description="ここはコンパスのメンバー募集や質問募集ができる掲示板です。"
 				openGraph={{
 					type: "website",
-					title: "",
-					description: "デフォルトの説明",
-					site_name: "サイトの名前",
-					url: "サイトのURL",
+					title: "コンパス募集掲示板",
+					description: "ここはコンパスのメンバー募集や質問募集ができる掲示板です。",
+					site_name: "コンパス掲示板",
+					url: "https://www.compass-forum.net",
 					images: [
 					 {
 					 　url: "https://www.example.ie/og-image-01.jpg",
