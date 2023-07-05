89364a4eb4626873b674cdbd26c873974c08c082
ryu-keitaro <dragon131020@gmail.com>
2023-07-05 10:57:41 +0900

    Merge remote changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,42 @@
-import '@/styles/globals.scss'
-import React, { useEffect } from 'react'
-import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side')
-    jssStyles?.parentElement?.removeChild(jssStyles)
-  }, [])
-
-  return (
-      <Component {...pageProps} />
-  )
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+	return (
+		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+			<DefaultSeo
+				defaultTitle="コンパス募集掲示板"
+				description="ここはコンパスのメンバー募集や質問募集ができる掲示板です。"
+				openGraph={{
+					type: "website",
+					title: "",
+					description: "デフォルトの説明",
+					site_name: "サイトの名前",
+					url: "サイトのURL",
+					images: [
+					 {
+					 　url: "https://www.example.ie/og-image-01.jpg",
+					   width: 800,
+					   height: 600,
+					   alt: 'Og Image Alt',
+					   type: 'image/jpeg',
+					 },
+					],
+				}}
+				twitter={{
+					handle: '@handle',
+					site: '@site',
+					cardType: "summary_large_image",
+				}}
+			/>
+      ...
+		</>
+	);
+};
