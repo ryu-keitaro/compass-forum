139404056e217d800aa122d034f378dca91b55c4
ryu-keitaro <dragon131020@gmail.com>
2023-07-07 03:21:59 +0900

    Merge remote changes

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -11,13 +11,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBE6W2TXtedCPj5ZoKQu2WIyVEBOMD0BIg",
-  authDomain: "test-nextjs-d6670.firebaseapp.com",
-  projectId: "test-nextjs-d6670",
-  storageBucket: "test-nextjs-d6670.appspot.com",
-  messagingSenderId: "531537153080",
-  appId: "1:531537153080:web:5b1656e3c43845d1baeacd",
-  measurementId: "G-KYVPHJTMQX"
+  // apiKey: "AIzaSyBE6W2TXtedCPj5ZoKQu2WIyVEBOMD0BIg",
+  // authDomain: "test-nextjs-d6670.firebaseapp.com",
+  // projectId: "test-nextjs-d6670",
+  // storageBucket: "test-nextjs-d6670.appspot.com",
+  // messagingSenderId: "531537153080",
+  // appId: "1:531537153080:web:5b1656e3c43845d1baeacd",
+  // measurementId: "G-KYVPHJTMQX"
 };
 
 // Initialize Firebase
