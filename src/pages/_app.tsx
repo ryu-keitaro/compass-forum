82bc14ffc8b5316722a91119a04104807d873659
ryu-keitaro <dragon131020@gmail.com>
2023-07-07 00:49:09 +0900

    Merge remote changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 					name="viewport"
 					content="minimum-scale=1, initial-scale=1, width=device-width"
 				/>
-				<link rel="icon" href="/favicon2.ico" />
+				<link rel="icon" href="/favicon.ico" />
 
 			</Head>
 			<DefaultSeo
