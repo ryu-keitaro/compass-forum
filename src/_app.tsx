163a07f1ae8ea706fe9b6946f59df4db6b720fb0
ryu-keitaro <dragon131020@gmail.com>
2023-07-06 10:49:00 +0900

    Merge remote changes

--- src/_app.tsx
+++ src/_app.tsx
@@ -24,11 +24,11 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 					url: "https://www.compass-forum.net",
 					images: [
 					 {
-					 　url: "https://www.example.ie/og-image-01.jpg",
-					   width: 800,
-					   height: 600,
-					   alt: 'Og Image Alt',
-					   type: 'image/jpeg',
+					 	url: "https://www.example.ie/og-image-01.jpg",
+						width: 800,
+						height: 600,
+						alt: 'Og Image Alt',
+						type: 'image/jpeg',
 					 },
 					],
 				}}
